1197c76fd7cc2f4d950d0a55da7d8b3d2aa2d52c
ycy <1369679110@qq.com>
2024-02-28 23:00:13 +0800

    第一次没有格式化代码提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const str = 'woainizuguo'
+</script>
+
+<template>
+  <div>
+    <h1>11111</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
+}
+</style>
